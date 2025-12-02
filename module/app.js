59494a1effa4b45c4d6a740b97f02727c74b5fc4
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2025-12-02 20:12:14 +0530

    first commit

---
+++ module/app.js
@@ -0,0 +1,14 @@
+// what is node js ?
+// node js is an open source, cross platform and server side js runtime enviroment.
+// it allows developers to run javascript on your comupter and system instead of outside the browser. 
+
+
+// const math=require("./math.js")
+
+// math.add(10,5)
+// math.sub(20,2)
+
+const fs=require("./fs");
+
+
+fs.write
