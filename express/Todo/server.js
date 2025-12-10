11772f604c8bb5abc847e55937cc86f041b704f0
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2025-12-10 22:50:53 +0530

    ....

--- express/Todo/server.js
+++ express/Todo/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import fs, { write } from 'fs'
 import path from 'path'
+import { json } from 'stream/consumers';
 import { fileURLToPath } from 'url'
 
 const app = express();
@@ -20,9 +21,30 @@ const WriteUser = (users) => {
     fs.writeFileSync(filepath, JSON.stringify(users))
 }
 
-// for Todo get
-app.get("/", (req, res) => {
+app.get("/",(req,res)=>{
+    const users=ReadUser();
+    const status=req.query.status;
+    const date=req.query.dueDate;
+    if(status){
+        const data=users.filter((Todo)=>Todo.status==status);
+        res.json([data]);
+    }else if(date){
+        const data = users.filter(todo => todo.dueDate==date);
+        res.json([data]);
+
+    }
+    res.json(users)
+})
+// for Todo get only single user using ID
+app.get("/:id", (req, res) => {
     const users = ReadUser();
+    const id=req.params.id;
+    if(id){
+
+        const data=users.filter((Todo)=>Todo.id==id)
+        res.json([data]);
+    }
+
     res.json(users)
 })
 
@@ -40,12 +62,12 @@ app.put("/", (req, res) => {
     const body = req.body;
     let userFound = false;
 
-    users = users.map((user) => {
-        if (user.id == Number(body.id)) {
+    users = users.map((Todo) => {
+        if (Todo.id == Number(body.id)) {
             userFound = true;
-            return { ...user, ...body };
+            return { ...Todo, ...body };
         }
-        return user;
+        return Todo;
     })
     if (!userFound) {
         res.json({ message: "user not found" });
@@ -58,7 +80,7 @@ app.put("/", (req, res) => {
 app.delete("/:id", (req, res) => {
     let users = ReadUser();
     const id = req.params.id;
-    users = users.filter((user) => user.id != Number(id))
+    users = users.filter((Todo) => Todo.id != Number(id))
     WriteUser(users)
     res.json({ message: "user deleted successfully" })
 })
