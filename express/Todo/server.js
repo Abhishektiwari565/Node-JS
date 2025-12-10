9d0395aa471f19a5ab7b86a47be1b6491a5e5817
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2025-12-10 16:34:20 +0530

    ...

--- express/Todo/server.js
+++ express/Todo/server.js
@@ -1,43 +1,72 @@
 import express from 'express'
-import fs from 'fs'
+import fs, { write } from 'fs'
 import path from 'path'
-import {fileURLToPath} from 'url'
+import { fileURLToPath } from 'url'
 
-const app=express();
+const app = express();
 app.use(express.json());
 
-const __filename=fileURLToPath(import.meta.url);
-const __dirname=path.dirname(__filename)
-const filepath=path.join(__dirname,"db.json")
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+const filepath = path.join(__dirname, "db.json")
 
-const ReadUser=()=>{
+const ReadUser = () => {
     console.log("Reading file:", filepath);
-    const data=fs.readFileSync(filepath,"utf-8");
+    const data = fs.readFileSync(filepath, "utf-8");
     return JSON.parse(data);
 }
 
-const WriteUser=(users)=>{
-    fs.writeFileSync(__filename,JSON.stringify(users))
+const WriteUser = (users) => {
+    fs.writeFileSync(filepath, JSON.stringify(users))
 }
 
 // for Todo get
-app.get("/",(req,res)=>{
-    const users=ReadUser();
+app.get("/", (req, res) => {
+    const users = ReadUser();
     res.json(users)
 })
 
 // for Todo post
-app.post("/",(req,res)=>{
-    const users=ReadUser();
+app.post("/", (req, res) => {
+    const users = ReadUser();
     users.push(req.body);
     WriteUser(users);
-    res.json({message:"user inserted"})
+    res.json({ message: "user inserted" })
+})
+
+// for Todo put
+app.put("/", (req, res) => {
+    let users = ReadUser();
+    const body = req.body;
+    let userFound = false;
+
+    users = users.map((user) => {
+        if (user.id == Number(body.id)) {
+            userFound = true;
+            return { ...user, ...body };
+        }
+        return user;
+    })
+    if (!userFound) {
+        res.json({ message: "user not found" });
+    }
+    WriteUser(users);
+    res.json({ message: "user updated successfully" });
+})
+
+// for Todo delete
+app.delete("/:id", (req, res) => {
+    let users = ReadUser();
+    const id = req.params.id;
+    users = users.filter((user) => user.id != Number(id))
+    WriteUser(users)
+    res.json({ message: "user deleted successfully" })
 })
 
 
 
 
 // for run server
-app.listen(2121,()=>{
+app.listen(2121, () => {
     console.log("server started");
 })
