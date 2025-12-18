af7e51f44c30e3cca4c4e3f98c4db8003f309b47
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2025-12-18 22:03:57 +0530

    .....

--- database/mongodb-driver/server.js
+++ database/mongodb-driver/server.js
@@ -1,4 +1,4 @@
-import {MongoClient} from 'mongodb'
+import {MongoClient,ObjectId} from 'mongodb'
 import express from 'express'
 
 const app=express();
@@ -12,23 +12,12 @@ const connectDB =async()=>{
     return db;
 }
 
-const addStudent=async()=>{
+const addStudent=async(student)=>{
     const db= await connectDB();
-    const result= await db.collection("primary").insertOne({
-        name:"John Doe",
-        age:10,
-        grade:"5th"
-     });
+    const result= await db.collection("primary").insertOne(student);
      return result;
 }
 
-const updateStudent=async()=>{
-    const db=await connectDB();
-    const id=req.params.id
-    const result=db.collection("primary").updateOne({
-
-    })
-}
 
 const readStudent=async()=>{
     const db=await connectDB();
@@ -37,13 +26,43 @@ const readStudent=async()=>{
     return data;
 }
 
+const updateStudent=async (id,student)=>{
+    const db=await connectDB();
+    const result=db.collection("primary").updateOne(
+        {_id:new ObjectId(id)},
+        {$set:student}
+    )
+}
+
+const deleteStudent=async(id)=>{
+    const db= await connectDB()
+    const result=await db.collection("primary").deleteOne({
+        _id:new ObjectId(id)
+    })
+}
+
 app.get('/api',async(req,res)=>{
     const data=await readStudent()
     res.json(data)
 });
 
-app.update("/api",async (req,res)=>{
-    const data=await updateStudent()
+app.post("/api",async (req,res)=>{
+   const student=req.body;
+   await addStudent(student);
+  res.json({message:"student added successfully"})
+})
+
+app.put("/api/:id",async (req,res)=>{
+    const id=req.params.id;
+    const student=req.body;
+    updateStudent(id,student);
+    res.json({message:"student updated succcessfully"});
+})
+
+app.delete("/api/:id",(req,res)=>{
+    const id=req.params.id;
+    deleteStudent(id);
+    res.json({message:"student deleted successfully"});
 })
 
 app.listen(4000,()=>{
@@ -52,4 +71,4 @@ app.listen(4000,()=>{
 
 
 // addStudent();
-readStudent();
+// readStudent();
