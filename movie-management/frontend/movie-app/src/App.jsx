08cc7259d12fe9b32ac6de392a4411f6229f6b14
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2025-12-27 14:12:59 +0530

    ....

--- movie-management/frontend/movie-app/src/App.jsx
+++ movie-management/frontend/movie-app/src/App.jsx
@@ -8,7 +8,8 @@ function App() {
   const [description, setDescription] = useState("")
   const [genre, setGenre] = useState("")
   const [releaseYear, setReleaseYear] = useState("")
-  const [moviePoster, setMoviePoster] = useState(null)
+  const [moviePoster, setMoviePoster] = useState("")
+  const [img,setImg]=useState("")
 
   const handleSubmit=async()=>{
     const formdata= new FormData()
@@ -20,20 +21,26 @@ function App() {
     try{
       const result=await axios.post("http://localhost:5000/",formdata)
       alert("movie added")
+      setImg("http://localhost:5000/uploads/"+result.data.data.moviePoster)
       
     }catch(err){
       alert("movie not added");
       console.log("Movie not found"+err)
     }
   }
+
+  
   return (
     <>
+    <img src={img} alt="" />
       <div><input type="text" placeholder='Enter movie title...' value={title} onChange={(e)=>setTitle(e.target.value)}  /></div>
       <div><input type="text" placeholder='Enter movie description...' value={description} onChange={(e)=>setDescription(e.target.value)} /></div>
       <div><input type="text" placeholder='Enter movie genre...' value={genre} onChange={(e)=>setGenre(e.target.value)} /></div>
       <div><input type="number" placeholder='Enter movie releaseYear...' value={releaseYear} onChange={(e)=>setReleaseYear(e.target.value)} /></div>
       <div><input type="file" onChange={(e)=>setMoviePoster(e.target.files[0])} /></div>
       <div><button onClick={handleSubmit}>Submit</button></div>
+      <div><button>Edit</button></div>
+      <div><button>Delete</button></div>
     </>
   )
 }
