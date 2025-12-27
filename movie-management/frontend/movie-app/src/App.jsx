b97b402698d11248b72e2940ddb67a04721724b6
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2025-12-27 22:57:36 +0530

    .....

--- movie-management/frontend/movie-app/src/App.jsx
+++ movie-management/frontend/movie-app/src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios';
 import './App.css'
 
@@ -10,6 +10,8 @@ function App() {
   const [releaseYear, setReleaseYear] = useState("")
   const [moviePoster, setMoviePoster] = useState("")
   const [img,setImg]=useState("")
+  const [fetchMovies,setFetchMovies]=useState([]);
+  const [movieId,setMovieId]=useState("")
 
   const handleSubmit=async()=>{
     const formdata= new FormData()
@@ -19,13 +21,40 @@ function App() {
     formdata.append("releaseYear",releaseYear);
     formdata.append("moviePoster",moviePoster);
     try{
-      const result=await axios.post("http://localhost:5000/",formdata)
-      alert("movie added")
-      setImg("http://localhost:5000/uploads/"+result.data.data.moviePoster)
+      const result=await axios.post("http://localhost:5000/",formdata);
+      alert("movie added");
+      setMovieId(result.data.data._id) ;
+      setImg("http://localhost:5000/uploads/"+result.data.data.moviePoster);
       
     }catch(err){
       alert("movie not added");
-      console.log("Movie not found"+err)
+      console.log("Movie not found"+err);
+    }
+  }
+
+
+  const getMovies=async()=>{
+    const res=await axios.get("http://localhost:5000/")
+    setFetchMovies(res.data);
+  }
+
+  useEffect(()=>{getMovies();},[]);
+
+  const handleDelete=async(id)=>{
+    try{
+      await axios.delete(`http://localhost:5000/${id}`);
+      console.log("Delete id:",id);
+      alert("movie deleted");
+      getMovies();
+      // setImg("");
+      // setDescription("");
+      // setGenre("");
+      // setMovieId("");
+      // setReleaseYear("");
+      // setTitle("");
+    }catch(err){
+      console.log("Movie not deleted"+err)
+      alert("movie not deleted")
     }
   }
 
@@ -39,8 +68,18 @@ function App() {
       <div><input type="number" placeholder='Enter movie releaseYear...' value={releaseYear} onChange={(e)=>setReleaseYear(e.target.value)} /></div>
       <div><input type="file" onChange={(e)=>setMoviePoster(e.target.files[0])} /></div>
       <div><button onClick={handleSubmit}>Submit</button></div>
-      <div><button>Edit</button></div>
-      <div><button>Delete</button></div>
+      <div><button >Edit</button></div>
+     
+
+      {fetchMovies.map((movie)=>(
+        <div key={movie._id}>
+          <h3>{movie.title}</h3>
+          <p>{movie.description}</p>
+          <p>{movie.genre}|{movie.releaseYear}</p>
+          <img src={`http://localhost:5000/uploads/${movie.moviePoster}`} alt="" />
+           <div><button onClick={()=>handleDelete(movie._id)}>Delete</button></div>
+        </div>
+      ))}
     </>
   )
 }
