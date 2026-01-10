8946ef7f2fc829e67736827ad2b164ec2afd6d30
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2026-01-10 12:29:45 +0530

    ...

---
+++ Blog/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+    <input type="text" placeholder="Enter email" />
+    <input type="text" placeholder="Enter password" />
+    </>
+  )
+}
+
+export default App
