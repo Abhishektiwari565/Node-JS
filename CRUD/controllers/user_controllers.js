ae1418ed9a9b8cb6e0a6465131ee796c706cb301
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2026-04-06 21:31:34 +0530

    ...

--- CRUD/controllers/user_controllers.js
+++ CRUD/controllers/user_controllers.js
@@ -6,7 +6,10 @@ export const signUp=async(req,res)=>{
         const {email,password}=req.body;
         const hashed=await bcrypt.hash(password,12)
         const user = await userCollection.create({email,password:hashed});
-        res.json({message:"User created successfully", user});
+        res.json({message:"User registered successfully",  user: {
+        id: user._id,
+        email: user.email,
+      },});
     }catch(err){
         res.json({message:err.message})
     }
@@ -18,17 +21,17 @@ export const signIn=async(req,res)=>{
     const user=await userCollection.findOne({email});
 
     if(!user){
-        res.json({message:"user not found !"});
+        return res.json({message:"user not found !"});
     }
     const isMatch=await bcrypt.compare(password,user.password);
     if(!isMatch){
-        res.json({message:"password is incorrect!!"});
+        return res.json({message:"password is incorrect!!"});
     }
 
     const token=jwt.sign(
         {id:user._id},
         process.env.JWT_SECRET,
-        {xpiresIn:"1d"}
+        {expiresIn:"1d"}
     );
     res.json({message:"login successfully",token,user:{
         id:user._id,
@@ -37,4 +40,55 @@ export const signIn=async(req,res)=>{
    }catch(err){
     res.json(err.message);
    }
+}
+
+export const getAllUsers=async(req,res)=>{
+    try{
+        const user=await userCollection.find();
+        res.json({messsage:"all users",user});
+    }catch(err){
+        res.json({message:err.message});
+    }
+}
+
+export const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { email, password } = req.body;
+
+    let updateData = {};
+
+    if (email) {
+      updateData.email = email;
+    }
+
+    if (password) {
+      const hashed = await bcrypt.hash(password, 12);
+      updateData.password = hashed;
+    }
+
+    const user = await userCollection.findByIdAndUpdate(
+      id,
+      updateData,
+      { new: true }
+    );
+
+    res.json({
+      message: "User updated successfully",
+      user
+    });
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export const deleteUser=async(req,res)=>{
+    const {id}=req.params;
+    try{
+        await userCollection.findByIdAndDelete(id);
+        res.json({message:"user deleted successfully!!"});
+    }catch(err){
+        res.json(err.message);
+    }
 }
--- CRUD/routes/user_routes.js
+++ CRUD/routes/user_routes.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import  {signUp,signIn}  from '../controllers/user_controllers.js';
+import  {signUp,signIn,getAllUsers,updateUser,deleteUser}  from '../controllers/user_controllers.js';
 
 const router=express.Router();
 router.post("/signup",signUp);
 router.post("/signin",signIn);
-
+router.get("/getUsers",getAllUsers);
+router.put("/update/:id",updateUser);
+router.delete("/delete/:id",deleteUser);
 export default router;
