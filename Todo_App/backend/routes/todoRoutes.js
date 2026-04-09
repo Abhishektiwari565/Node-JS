c8d6064f541eea2eaa6db3b778733f800ec4a22a
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2026-04-09 10:00:39 +0530

    ....

--- Todo_App/backend/routes/todoRoutes.js
+++ Todo_App/backend/routes/todoRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import {getTodo,addTodo,deleteTodo} from '../controllers/todoControllers.js'
+import {authMiddleware} from './middleware/authMiddleware.js'
 
 const router=express.Router();
-router.get("/addtodo",addTodo);
-router.get("/gettodo",getTodo);
-router.get("/deletetodo/:id",deleteTodo);
+router.get("/addtodo",authMiddleware,addTodo);
+router.get("/gettodo",authMiddleware,getTodo);
+router.get("/deletetodo/:id",authMiddleware,deleteTodo);
 
 export default router;
